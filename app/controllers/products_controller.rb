f8d29ead38f8153f2f55061d3eea7da23bcec64a
ciou <yyouko22@gmail.com>
2017-06-15 21:28:11 +0800

    fix add_to_cart bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,6 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
 
-    current_cart.add_product_to_cart(@product)
-
     if !current_cart.products.include?(@product)
       current_cart.add_product_to_cart(@product)
       flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
