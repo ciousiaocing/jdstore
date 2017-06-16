9339f2ddbe977c4744b89d8eca1b48bc35e8a974
ciou <yyouko22@gmail.com>
2017-06-16 12:43:02 +0800

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
-u = User.new
-u.email = "admin@test.com"
 
-u.password = "123456"
+# Initialize Admin User
+if User.find_by(email: "admin@test.com").nil?
+  u = User.new
+  u.email = "admin@test.com"
+  u.password = "123456"
+  u.password_confirmation = "123456"
+  u.is_admin = true
+  u.save
+  puts "Admin 已經建立好了，帳號為#{u.email}, 密碼為#{u.password}"
+else
+  puts "Admin 已經建立過了，腳本跳過該步驟。"
+end
 
-u.password_confirmation = "123456"
 
-u.is_admin = true
-u.save
+# Initialize Product
+
+Product.create!(title: "QC-35",
+  description: "耳機",
+  price: 2800,
+  quantity: 5,
+  image: open("https://images-cn.ssl-images-amazon.com/images/I/41bVSMLUllL._AC_UL320_SR320,320_.jpg")
+  )
+
+Product.create!(title: "Iphone7",
+  description: "手機",
+  price: 5000,
+  quantity: 5,
+  image: open("https://images-cn.ssl-images-amazon.com/images/I/51q3gdJGenL._SL800_.jpg")
+  )
+
+Product.create!(title: "Cherry Keyboard",
+  description: "G80-3000鍵盤",
+  price: 2800,
+  quantity: 5,
+  image: open("https://images-cn.ssl-images-amazon.com/images/I/41LN-bXtA7L._AC_UL320_SR320,320_.jpg")
+  )
