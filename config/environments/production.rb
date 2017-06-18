aa0eaa289d520f5c5fe67a641a2490d53541177b
ciou <yyouko22@gmail.com>
2017-06-18 21:18:04 +0800

    change heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://murmuring-ravine-86325.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/jdstore-ciou.git' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
